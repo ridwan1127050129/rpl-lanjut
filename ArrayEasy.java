4d7d253e48429efe03e873d50ef854046d0d57f3
unknown <abdulonex@gmail.com>
2014-09-15 15:37:22 +0700

    file ketiga..

---
+++ ArrayEasy.java
@@ -0,0 +1,8 @@
+public class ArrayEasy{
+         public static void main(String[]args){
+                    int[]hit = new int [10];
+                          for(int i=1; i<=10; i++){
+                                System.out.println(i+ " tambah " +i+ " = " +(i+i));
+	}
+       }
+}
